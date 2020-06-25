5ecfb0b7d56470f65c032f878570dcb97483c320
Angel Nikolov <ang.n.nikolov@gmail.com>
2020-06-25 22:48:06 +0300

    Initial upload

---
+++ AsyncSubmit/AsyncSubmit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AsyncSubmit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
