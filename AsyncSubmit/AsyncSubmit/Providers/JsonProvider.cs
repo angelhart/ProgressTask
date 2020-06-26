5cbcfd0889c2f34f8e4ed17ca660b31202c5e3e0
Angel Nikolov <ang.n.nikolov@gmail.com>
2020-06-26 13:55:59 +0300

    Created json serializer class

---
+++ AsyncSubmit/AsyncSubmit/Providers/JsonProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AsyncSubmit.Providers
+{
+    public static class JsonProvider<T>
+    {
+        private static readonly JsonSerializerOptions jsonOptions = new JsonSerializerOptions { DictionaryKeyPolicy = JsonNamingPolicy.CamelCase };
+
+        public static StringContent Serialize(T model)
+        {
+            return new StringContent(
+                JsonSerializer.Serialize(model, jsonOptions),
+                Encoding.UTF8,
+                "application/json");
+        }
+    }
+}
