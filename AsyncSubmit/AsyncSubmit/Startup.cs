c50e4f0ab279435a98791f87b789cd3eef1660c1
Angel Nikolov <ang.n.nikolov@gmail.com>
2020-06-29 12:18:29 +0300

    Registered IDataParser in StartUp Services

--- AsyncSubmit/AsyncSubmit/Startup.cs
+++ AsyncSubmit/AsyncSubmit/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AsyncSubmit.Providers;
+using AsyncSubmit.Providers.Contracts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -20,15 +22,15 @@ namespace AsyncSubmit
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
 
             services.AddHttpClient();
+
+            services.AddSingleton<IDataParser, JsonProvider>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -38,7 +40,6 @@ namespace AsyncSubmit
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
