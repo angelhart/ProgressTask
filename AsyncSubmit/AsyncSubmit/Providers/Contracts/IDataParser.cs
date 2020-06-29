9ccd27f7ce36ca3e0668bd107e5418b87f10f8fd
Angel Nikolov <ang.n.nikolov@gmail.com>
2020-06-29 12:13:28 +0300

    Made JsonProvider implement new IdataParser interface

---
+++ AsyncSubmit/AsyncSubmit/Providers/Contracts/IDataParser.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+
+namespace AsyncSubmit.Providers.Contracts
+{
+    public interface IDataParser
+    {
+        StringContent Serialize<T>(T model);
+    }
+}
--- AsyncSubmit/AsyncSubmit/Providers/JsonProvider.cs
+++ AsyncSubmit/AsyncSubmit/Providers/JsonProvider.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using AsyncSubmit.Providers.Contracts;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace AsyncSubmit.Providers
 {
-    public static class JsonProvider<T>
+    public class JsonProvider : IDataParser
     {
         private static readonly JsonSerializerOptions jsonOptions = new JsonSerializerOptions { DictionaryKeyPolicy = JsonNamingPolicy.CamelCase };
 
-        public static StringContent Serialize(T model)
+        public StringContent Serialize<T>(T model)
         {
             return new StringContent(
                 JsonSerializer.Serialize(model, jsonOptions),
